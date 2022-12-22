a0bb0f926892825a8555bf9dd5533975815e3970
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-12-22 23:31:01 +0630

    added cursor circles

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,7 @@ const selectEl = selector => {
     throw new Error(`Something went wrong, make sure that $(selector) exists or is typed correctly.`);
 }
 
+//nav-bar-shadow
 const scrollHeader = () => {
     const headerEl = selectEl('#header');
     console.log(this.scrollY)
@@ -14,5 +15,47 @@ const scrollHeader = () => {
     }
     
 }
-
 window.addEventListener('scroll', scrollHeader);
+
+//circles 
+let coords = {x:0, y:0};
+const circlesEl = document.querySelectorAll('.circles');
+
+circlesEl.forEach(function(circleEl) {
+    circleEl.x = 0;
+    circleEl.y = 0;
+})
+
+window.addEventListener('mousemove', function(e) {
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+})
+
+const animateCircles = () => {
+    let x = coords.x;
+    let y = coords.y;
+
+    circlesEl.forEach(function(circleEl, i) {
+        circleEl.style.left = x + 'px';
+        circleEl.style.top = y + 'px';
+
+        circleEl.style.scale = (circlesEl.length - i) / circlesEl.length; 
+
+        circleEl.x = x;
+        circleEl.y = y;
+
+        const nextCircle = circlesEl[i + 1] || circlesEl[0];
+        x += (nextCircle.x - x) * 0.3;
+        y += (nextCircle.y - y) * 0.3;
+    })
+    requestAnimationFrame(animateCircles);
+}
+animateCircles();
+
+const colors = ['#f2f2f2', '#e6e6e6', '#d9d9d9', '#cccccc', '#bfbfbf', '#b3b3b3', '#a6a6a6', '#999999', '#8c8c8c'];
+
+circlesEl.forEach(function(circleEl, i) {
+    circleEl.style.backgroundColor = colors[i];
+    circleEl.style.zIndex = -i;
+})
+
